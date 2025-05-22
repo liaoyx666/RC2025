d2218c02ba83864155441a6410c69b5e6eb7dcac
千早爱音 <2376887937@qq.com>
2025-05-22 23:45:00 +0800

    Update Launch_Speed.cpp

--- SWERVE_play/GDUTRCLIB/Components/Src/Launch_Speed.cpp
+++ SWERVE_play/GDUTRCLIB/Components/Src/Launch_Speed.cpp
@@ -1,12 +1,19 @@
 #include "Launch_Speed.h"
-#include "math.h"
 
-float Get_Launch_RPM(float d)
-{
-	float Speed;
-	Speed = (g*d*d)/((2 * cos(a) * cos(a)) *(d * tan(a) + 76.72f - H - ((k * d * d * sin(a))/(2*cos(a)*cos(a)))));
-        float RPM = (sqrt(Speed) * 60 * 19) / (2 * pi * r);
-	return RPM;
-    
+double calculate_launch_speed(double angle_deg, double distance) {
+    double angle_rad = DEG_TO_RAD(angle_deg);
+    double h = HOOP_HEIGHT - LAUNCH_HEIGHT;
+    double d = distance;
 
+    double numerator = GRAVITY * d * d;
+    double denominator = 2.0 * pow(cos(angle_rad), 2.0) * (d * tan(angle_rad) - h);
+
+    double v = sqrt(numerator / denominator);
+    v *= (1.0 + AIR_RESISTANCE_COEFF);  // 考虑空气阻力
+
+    double rpm = v * MOTOR_POLE_PAIRS * 60.0 / (2.0 * M_PI * MOTOR_RADIUS);
+    return rpm;
 }
+
+
+
