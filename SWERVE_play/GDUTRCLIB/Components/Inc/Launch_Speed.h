b1c52b457c9392f09d4a3acbff30c7867f44aae3
千早爱音 <2376887937@qq.com>
2025-05-22 13:18:25 +0800

    Add files via upload
    增加了根据距离调整发射速度的功能

---
+++ SWERVE_play/GDUTRCLIB/Components/Inc/Launch_Speed.h
@@ -0,0 +1,12 @@
+#ifdef __cplusplus
+#define pi 3.1415926927f
+#define H 243.0f           //篮筐高度
+#define a 70*pi/180        //机构抬升角度
+#define k 0.0223f           //阻力系数
+#define g 9.7883f          //重力加速度
+
+float Get_Launch_RPM(float d);
+
+
+#endif
+
