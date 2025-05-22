f49d6ea431d3af22270ddef23aa8a39bbc8a3ab1
千早爱音 <2376887937@qq.com>
2025-05-22 16:06:41 +0800

    Update Launch_Speed.h

--- SWERVE_play/GDUTRCLIB/Components/Inc/Launch_Speed.h
+++ SWERVE_play/GDUTRCLIB/Components/Inc/Launch_Speed.h
@@ -1,10 +1,10 @@
 #ifdef __cplusplus
 #define pi 3.1415926927f
-#define H 243.0f           //篮筐高度
+#define H 243.0f           //篮筐高度cm
 #define a 70*pi/180        //机构抬升角度
-#define k 0.0223f           //阻力系数
-#define g 9.7883f          //重力加速度
-
+#define k 0.00000127f           //阻力系数
+#define g 978.83f          //重力加速度
+#define r 4.05f  
 float Get_Launch_RPM(float d);
 
 
